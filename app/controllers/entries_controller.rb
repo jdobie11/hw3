26a45577f059b80d298d537b65e4c054e2aed8cb
jdobie11 dobes <jonathon.dobie@kellogg.northwestern.edu>
2025-02-26 03:48:58 +0000

    everything but the date

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,29 +1,26 @@
 class EntriesController < ApplicationController
-  def show
-    @entry = Entry.find(params[:id])
-    @place = Place.find_by({"id" => @entry["place_id"]})
-    # render contacts/show view with details about Contact
-  end
-
-  def new
-    @place = Place.find(params[:place_id])
-    @entry = Entry.new
-  end
 
   def create
-    @place = Place.find(params[:place_id])
-    @entry = @place.entries.build(entry_params)
+
+    @entry = Entry.new
+
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["date"] = params["date"]
+
+    @entry["place_id"] = params["place_id"]
 
     if @entry.save
-      redirect_to place_path(@place), notice: 'Entry was successfully created.'
+      redirect_to "/places/#{@entry["place_id"]}"
     else
-      render :new
+
+      @place = Place.find_by({ "id" => params["place_id"] }) 
+      render :new  
     end
   end
 
-  private
-
-  def entry_params
-    params.require(:entry).permit(:title, :description, :date)
+  def new
+    @place = Place.find_by({ "id" => params["place_id"] })
   end
+
 end
