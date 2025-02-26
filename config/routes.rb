bb3c9317aa43447ded22a22901565e0aa7066238
jdobie11 dobes <jonathon.dobie@kellogg.northwestern.edu>
2025-02-26 02:57:01 +0000

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ 
+  get("/places", { :controller => "places", :action => "index" })
+  get("/places/new", { :controller => "places", :action => "new" })
+  get("/places/:id", { :controller => "places", :action => "show" })
+  post("/places", { :controller => "places", :action => "create" })
 
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
 
-    get("/", {:controller => "places", :action => "index"})
-    
-    get("/places", {:controller => "places", :action => "index"})
-    get("/places/new", {:controller => "places", :action => "new"})
-    get("/places/:id", {:controller => "places", :action => "show"})
-    post("/places", {:controller => "places", :action => "create"})
-  
-    get("/places/:place_id/entries/new", {:controller => "entries", :action => "new"})
-    post("/places/:place_id/entries", {:controller => "entries", :action => "create"})
-  
-  
+  get("/entries/new", { :controller => "entries", :action => "new" })
+  post("/entries", { :controller => "entries", :action => "create" })
 end
+
